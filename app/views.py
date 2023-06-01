c7b38d8853bcea41e7664582209b4a38cfcfce27
SivaLingala2000 <sivakumarlingala2000@gmail.com>
2023-06-01 20:09:00 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def madhav(request):
+    return render(request,'madhav.html')
+
+def siva(request):
+    return render(request,'siva.html')
